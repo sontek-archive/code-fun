4be96fb1fdafd93c21095d9099c44e23c54498b9
John Anderson <sontek@gmail.com>
2012-10-23 01:02:28 -0300

    allow things to rely on each other

---
+++ python-circular/A.py
@@ -0,0 +1,9 @@
+import B
+
+def D():
+    B.C()
+
+def hello():
+    print "B"
+    D()
+
---
+++ python-circular/B.py
@@ -0,0 +1,12 @@
+import A
+
+def hello():
+    print "A"
+    A.hello()
+
+def C():
+    print "C"
+
+if __name__ == '__main__':
+    hello()
+
